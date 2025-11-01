71a2f2397fbc1532837f5036a490801f279b6c5f
anumcait <anumcait@gmail.com>
2025-11-01 11:30:18 +0530

    gcloud build test

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const port = process.env.PORT || 8080;
 const server = http.createServer((req, res) => {
-  res.end('Hello from Google Cloud Build!');
+  res.end('Hello from Google Cloud Build - Here!');
 });
 server.listen(port, () => console.log(`App running on port ${port}`));
